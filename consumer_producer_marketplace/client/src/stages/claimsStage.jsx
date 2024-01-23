7e374229d93d914e9628b745d9375513a4d25e67
Abhishek Shah <theartofcoding2048@gmail.com>
2024-01-23 10:00:52 -0500

    Fixed bug where ad selection not processed

--- consumer_producer_marketplace/client/src/stages/claimsStage.jsx
+++ consumer_producer_marketplace/client/src/stages/claimsStage.jsx
@@ -238,7 +238,7 @@ export function ClaimsStage() {
 
 
   const handleSubmit = () => {
-    if (role === "producer" && selectedIdx) {
+    if (role === "producer" && selectedIdx !== -1) {
         player.round.set("productPrice", selectedIdx === 0 ? 3 : 7)
       const productCost = player.round.get("productCost");
       const unitsCanProduce = Math.floor(capital / productCost);
