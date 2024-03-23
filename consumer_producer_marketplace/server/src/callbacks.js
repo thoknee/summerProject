fd52c3638d599290ef7718d2805194d5e0314c43
thoknee <antonyp@bu.edu>
2024-03-23 14:51:49 -0400

    Added some early implementation of the informed consumer LLM

--- consumer_producer_marketplace/server/src/callbacks.js
+++ consumer_producer_marketplace/server/src/callbacks.js
@@ -487,6 +487,118 @@ async function updateProducerScores(game) {
       console.log(others);
       game.set("agents", others);
     }
+    
+    //Beginning work on the "informed AI"
+    if (consumerAgent.strategy == "informed") {
+      
+      //The first round is trivial for this and is essentially the same as titfortat and gullible
+      if (roundNum == 1) { 
+        let wallet = consumerAgent.wallet;
+        const mockQuantity = parseInt(wallet / productPrice);
+        const soldStock = mockQuantity <= remainingStock ? mockQuantity : remainingStock
+      
+
+      if (soldStock == 0) {
+        const totalCost = initialStock * productCost;
+        const totalSales = soldStock * productPrice;
+        const originalScore = player.get("score") || 0;
+        let score = player.get("score") || 0;
+        score += (totalSales - totalCost);
+        consumerAgent.purchaseHistory.push({
+          productQuality: productQuality,
+          productAdQuality: productAdQuality,
+          quantity: 0,
+          round: round,
+          roundNum: roundNum,
+        });
+        let consumerScore = consumerAgent.score;
+        consumerAgent.scores.push({
+          score: consumerScore,
+          round: round,
+          roundNum: roundNum
+        });
+        others.forEach(producerAgent => {
+          producerAgent.scores.push({
+            score: score,
+            round: round,
+            roundNum: roundNum
+          });
+          producerAgent.productionHistory.push({
+            productQuality: productQuality,
+            productAdQuality: productAdQuality,
+            initialStock: initialStock,
+            remainingStock: remainingStock,
+            soldStock: soldStock,
+            round: round,
+            roundNum: roundNum
+          })
+        });
+        let cheated = productAdQuality === productQuality ? false : productAdQuality === "low" && productQuality === "high" ? false : true
+        consumerAgent.cheatedHistory.push(cheated)
+        player.set("score", score);
+        player.set("scoreDiff", score - originalScore);
+        player.set("capital", capital + totalSales);
+      }
+      else {
+        const trialStock = tempStock.map((item) => {
+          return item.round === round
+            ? {
+              ...item,
+              remainingStock: item.remainingStock - soldStock,
+              soldStock: item.soldStock + soldStock,
+            }
+            : item;
+        });
+
+        player.set("stock", trialStock);
+        const totalCost = initialStock * productCost;
+        const totalSales = soldStock * productPrice;
+        const originalScore = player.get("score") || 0;
+        let score = player.get("score") || 0;
+        score += (totalSales - totalCost);
+
+        consumerAgent.purchaseHistory.push({
+          productQuality: productQuality,
+          productAdQuality: productAdQuality,
+          quantity: soldStock,
+          round: round,
+          roundNum: roundNum,
+        });
+        let consumerScore = consumerAgent.score;
+        consumerScore = (value - productPrice) * soldStock;
+        consumerAgent.score = consumerScore;
+        consumerAgent.scores.push({
+          score: consumerScore,
+          round: round,
+          roundNum: roundNum
+        });
+        others.forEach(producerAgent => {
+          producerAgent.scores.push({
+            score: score,
+            round: round,
+            roundNum: roundNum
+          });
+          producerAgent.productionHistory.push({
+            productQuality: productQuality,
+            productAdQuality: productAdQuality,
+            initialStock: initialStock,
+            remainingStock: remainingStock,
+            soldStock: soldStock,
+            round: round,
+            roundNum: roundNum
+          })
+        });
+        let cheated = productAdQuality === productQuality ? false : productAdQuality === "low" && productQuality === "high" ? false : true
+        consumerAgent.cheatedHistory.push(cheated)
+        wallet = wallet - parseInt(productPrice * soldStock);
+        consumerAgent.wallet = wallet;
+        player.set("score", score);
+        player.set("scoreDiff", score - originalScore);
+        player.set("capital", capital + totalSales);
+      }
+
+      }
+  }
   });
 }
 
