3fb3140c8857652e4c2e98ca73f408b7cd6c2911
thoknee <antonyp@bu.edu>
2024-03-23 15:20:17 -0400

    Added the informed option into the consumerAgent

--- consumer_producer_marketplace/client/src/strategies/ConsumerAgent.js
+++ consumer_producer_marketplace/client/src/strategies/ConsumerAgent.js
@@ -39,12 +39,26 @@ export class TitforTat extends ConsumerAgent {
     }
 }
 
+class Informed extends ConsumerAgent{
+    purchaseQuantity(wallet, productPrice){
+        return Math.floor(wallet/productPrice)
+    }
+}
+
+export class Informed extends ConsumerAgent {
+    purchaseQuantity(wallet, productPrice){
+        return Math.floor(wallet/productPrice)
+    }
+}
+
 export function getConsumerAgentFromId(id) {
     switch (id) {
         case "Gullible":
             return new Gullible()
         case "Tit for tat":
             return new TitforTat()
+        case "Informed":
+            return new Informed();
         default:
             console.log("No bot found with that ID");
     }
